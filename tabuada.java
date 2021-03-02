b39047b0cafd37a7699c887ae3b33270a093ef19
Thays Ribeiro <57158593+ThaysRibeiro@users.noreply.github.com>
2021-03-02 11:40:58 -0300

    Add files via upload

---
+++ tabuada.java
@@ -0,0 +1,21 @@
+/* Thays dos Santos Barroso Ribeiro 
+   Matrícula:0050006852*/
+
+
+
+public class tabuada {
+	
+		public static void main(String[] args) {
+			
+			for (int i = 0; i<=10; i++) {
+				System.out.println ("10 x " + i + " = " + (10*i));
+				}
+			}
+
+		}
+
+
+
+
+
+
